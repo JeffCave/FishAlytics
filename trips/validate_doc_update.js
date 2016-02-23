e64b4357929901001ffbc83fdf0592c9a4cd52fa
Jeff Cave <jefferey.cave@gmail.com>
2016-02-22 20:00:58 -0400

    Committing something or other.

--- trips/validate_doc_update.js
+++ trips/validate_doc_update.js
@@ -1,74 +1,110 @@
- 
+/**
+ * Validations for Trip documents
+ */ 
 function(newDoc, oldDoc, userCtx, secObj) {
-	var isChanged = (function(oldDoc,newDoc){
-			var o = JSON.parse(JSON.stringify(oldDoc));
-			var n = JSON.parse(JSON.stringify(newDoc));
-			o._rev = "";
-			o._revisions = "";
-			n._rev = "";
-			n._revisions = "";
-			o = JSON.stringify(o);
-			n = JSON.stringify(n);
-			
-			return (n !== o);
-		})(oldDoc,newDoc);
-	if(!isChanged){
-		throw({forbidden : "Entity is unchanged. Nothing to do."});
+	if('trip' != newDoc._id.split('.')[0].toLowerCase()){
+		return;
 	}
 	
-	var isAdmin = (function(userCtx, secObj){
-			isadmin = (userCtx.roles.indexOf('_admin') !== -1)
-				|| (
-					secObj && secObj.admins && secObj.admins.names 
-					&& secObj.admins.names.indexOf(userCtx.name) !== -1
-				);
-			// see if the user a database admin specified by role
-			if (!isadmin && secObj && secObj.admins && secObj.admins.roles){
-				var db_roles = secObj.admins.roles;
-				for(var idx = 0; idx < userCtx.roles.length; idx++) {
-					var user_role = userCtx.roles[idx];
-					if(db_roles.indexOf(user_role) !== -1) {
-						return true;
-					}
+	
+	//*******************************************************************
+	//* Helpers
+	var Validators = {
+		'waypoint' : function(waypoint){
+			//must be a valid date
+			$val = Date.parse(waypoint.timestamp);
+			if(!$val){
+				throw({ forbidden : 'Waypoint specifies an invalid timestamp: ' + waypoint.timestamp });
+			}
+			//location is optional
+			if(waypoint.coords){
+				//if you do specify a location, we need at least LAT and LONG
+				$val = parseFloat(waypoint.coords.longitude);
+				if(isNaN($val)){
+					throw({ forbidden : 'Waypoint specifies an invalid longitude: ' + $val });
+				}
+				if($val < -180 || $val > 180){
+					throw({ forbidden : 'Waypoint specifies an invalid longitude: ' + $val });
+				}
+				$val = parseFloat(waypoint.coords.latitude);
+				if(isNaN($val)){
+					throw({ forbidden : 'Waypoint specifies an invalid latitude: ' + $val });
+				}
+				if($val < -90 || $val > 90){
+					throw({ forbidden : 'Waypoint specifies an invalid latitude: ' + $val });
 				}
 			}
-			return isadmin;
-		})(userCtx, secObj)
-		;
+		},
+		'catch' : function(ctch){
+			//timestamp is optional on a catch
+			if(ctch.timestamp){
+ 				//must be a valid date
+ 				$val = Date.parse(ctch.timestamp);
+ 				if(!$val){
+ 					throw({ forbidden : 'Catch specifies and invalid timestamp: ' + $val });
+ 				}
+			}
+			//location is optional
+			if(ctch.coords){
+				$msg = "Catch locations require a '{field}' field ";
+				['longitude','latitude'].forEach(function(field){
+						if(!ctch.coords[field]){
+							throw({ forbidden : $msg.replace('{field}',field) });
+						}
+					});
+				//if you do specify a location, we need at least LAT and LONG
+				$val = parseFloat(ctch.coords.longitude);
+				if(isNaN($val)){
+					throw({ forbidden : 'Catch specifies an invalid longitude: ' + $val });
+				}
+				if($val < -180 || $val > 180){
+					throw({ forbidden : 'Catch specifies an invalid longitude: ' + $val });
+				}
+				$val = parseFloat(ctch.coords.latitude);
+				if(isNaN($val)){
+					throw({ forbidden : 'Catch specifies an invalid latitude: ' + $val });
+				}
+				if($val < -90 || $val > 90){
+					throw({ forbidden : 'Catch [' + c + '] specifies an invalid latitude: ' + $val });
+				}
+			}
+		}
+	};
 	
-	//META-DATA enforcement
-	if(!newDoc.meta){
-		throw({forbidden : "Entities are expected to maintain meta data"});
-	}
-	var meta = newDoc.meta;
-	var msg = "META-DATA: requires '{field}' field ";
-	['created','modified','by'].forEach(function(field){
-			if(!meta[field]){
+	//*******************************************************************
+	//* Required fields
+	var msg = "All trips require a '{field}' field ";
+	['fisherman','catches','waypoints'].forEach(function(field){
+			if(!newDoc[field]){
 				throw({ forbidden : msg.replace('{field}',field) });
 			}
 		});
-	$val = Date.parse(meta.created);
-	if(isNaN($val)){
-		throw({forbidden : "Creation date must be a date"});
-	}
-	if($val > Date.now()){
-		throw({forbidden : "Creation date cannot be in the future"});
-	}
-	$val = Date.parse(meta.modified);
-	if(Date.parse(meta.modified) > Date.now()){
-		throw({forbidden : "Modified date cannot be in the future"});
-	}
-//	if(!allusers.contains(meta.by)){
-//		throw({forbidden : "FK: data modified by unknown user"});
-//	}
 	
-	// UPDATE Enforcement
-	if(oldDoc){
-		if (oldDoc.meta){
-			//META DATA
-			if(oldDoc.meta.created && toJSON(oldDoc.meta.created) != toJSON(newDoc.meta.created)){
-				throw({ forbidden : "May not change creation timestamp" });
-			}
-		}
+	
+	//*******************************************************************
+	//* Fisherman must be constrained to known users
+	
+	
+	//*******************************************************************
+	//* Catches must be valid definition
+	if(!Array.isArray(newDoc.catches)){
+		throw({ forbidden : 'catches must be an array' });
+	}
+	for(var c=newDoc.catches.length-1; c>=0; c--){
+		var fish = newDoc.catches[c];
+		Validators['catch'](fish);
+	}
+	
+	// *******************************************************************
+	// * Waypoints must be valid definition
+	if(!Array.isArray(newDoc.waypoints)){
+		throw({ forbidden : 'Waypoints must be an array' });
+	}
+	if(newDoc.waypoints.length < 2){
+		throw({ forbidden : 'A trip must contain at least a start and end time' });
+	}
+	for(var w=newDoc.waypoints.length-1; w>=0; w--){
+		var waypoint = newDoc.waypoints[w];
+		Validators.waypoint(waypoint);
 	}
 }
