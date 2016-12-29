236e0261f7d9db4c49b6d099c49fe63992547f68
Jeff Cave <jefferey.cave@gmail.com>
2016-12-29 07:01:13 -0400

    Adding a sstatic landing page. This is to reduce problems with robots.

--- couchapp/trips/views/trips/map.js
+++ couchapp/trips/views/trips/map.js
@@ -6,8 +6,16 @@ function(doc) {
 	var d = JSON.parse(JSON.stringify(doc));
 	var date = null;
 	
+	d.location = d.location || d._id;
+	
 	d.start = Date.parse("9000-01-01");
 	d.finish = Date.parse("1970-11-01");
+	d.fish = {
+		'total' : d.catches.length,
+		'length' : 0,
+		'weight' : 0,
+		'freq' : {}
+	};
 	d.catches.concat(d.waypoints).forEach(function(e){
 		date = Date.parse(e.timestamp);
 		if(isNaN(date)) return;
@@ -17,7 +25,24 @@ function(doc) {
 		if(date > d.finish){
 			d.finish = date;
 		}
+		if(e.fish){
+			d.fish.freq[e.fish.species] = (d.fish.freq[e.fish.species] || 0)+1;
+			d.fish.weight += e.fish.weight;
+			d.fish.length += e.fish.length;
+		}
 	});
+	
+	
+	var maxFish = null;
+	for(var f in d.fish.freq){
+		maxFish = maxFish || {'name':f,'count':d.fish.freq[f]};
+		if(maxFish.count < d.fish.freq[f]){
+			maxFish.name = f;
+			maxFish.count = d.fish.freq[f];
+		}
+	}
+	d.fish.freq = maxFish.name;
+	
 	
 	date = new Date(d.start);
 	var key = [
