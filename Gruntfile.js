7d1231d7c2c67d8356f47900027195899196da53
Jefferey Cave <jefferey.cave@gmail.com>
2017-03-01 01:11:36 +0000

    Updating to allow for auto-push to production

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,97 +1,126 @@
-
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    checkDependencies: {
-        this: {
-            options: {
-                install: true,
-            },
-        },
-    },
-    jshint : {
-      files : ['Gruntfile.js', 'couchapp/**/*.js'],
-      options : {
-        ignores : [
-            'couchapp/*/lib/*',
-            'couchapp/trips/_attachments/scripts/regression.js',
-            'couchapp/trips/_attachments/scripts/leaflet.js',
-            'couchapp/triggerjob/**',
-          ],
-        esversion : 6,
-        //strict : 'implied',
-        laxcomma : true,
-        globals : {
-          couch: true
-        }
-      }
-    },
-    watch: {
-      files: ['<%= jshint.files %>'],
-      tasks: ['build']
-    },
-    'couch-compile': {
-      app: {
-        files: {
-          'bin/alldata.json': 'couchapp/alldata',
-          'bin/licenses.json': 'couchapp/licenses',
-          //'bin/triggerjob.json' : 'couchapp/triggerjob/*',
-          'bin/trips.json' : 'couchapp/trips',
-        }
-      }
-    },
-    'couch-push': {
-        //options: {user: 'karin',pass: 'secure'},
-        dev: {
-          files: [
-            {dest:'http://localhost:8080/fish', src:'bin/alldata.json'},
-            {dest:'http://localhost:8080/fish', src:'bin/licenses.json'},
-            //{dest:'http://localhost:8080/fish', src:'bin/triggerjob.json'},
-            {dest:'http://localhost:8080/fish', src:'bin/trips.json'}
-          ]
-        },
-        prod:{
-          files: [
-            {dest:'http://localhost:8080/fish', src:'bin/alldata.json'},
-            {dest:'http://localhost:8080/fish', src:'bin/licenses.json'},
-            //{dest:'http://localhost:8080/fish', src:'bin/triggerjob.json'},
-            {dest:'http://localhost:8080/fish', src:'bin/trips.json'}
-          ]
-        }
-    },
-    mochaTest: {
-        test: {
-          options: {
-            reporter: 'spec',
-            quiet: false, // Optionally suppress output to standard out (defaults to false) 
-            clearRequireCache: false // Optionally clear the require cache before running tests (defaults to false) 
-          },
-          src: ['test/**/*.js']
-        }
-    }
-  });
-  
-  
-  grunt.loadNpmTasks('grunt-check-dependencies');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-exec');
-  grunt.loadNpmTasks('grunt-couch');
-  grunt.loadNpmTasks('grunt-shell');
-  grunt.loadNpmTasks('grunt-mocha-test');
-  
-  grunt.registerTask('default', ['build']);
-  grunt.registerTask('test', ['mochaTest']);
-  grunt.registerTask('config',[
-    'checkDependencies',
-    'couch-configure',
-    ]);
-  grunt.registerTask('build',[
-    'checkDependencies',
-    'jshint',
-    'couch',
-    ]);
-  
+	grunt.initConfig({});
+	grunt.config.merge({
+		localTarget: {
+			url: 'http://localhost:8080/fish'
+		}
+	});
+	grunt.config.merge({
+		prodTarget: (() => {
+			try {
+				grunt.log.writeln(process.env.deployProd);
+				return JSON.parse(process.env.deployProd);
+			}
+			catch (e) {
+				return grunt.config.get('localTarget');
+			}
+		})(),
+	});
+	grunt.config.merge({
+		checkDependencies: {
+			this: {
+				options: {
+					install: true,
+				},
+			},
+		},
+		jshint: {
+			files: ['Gruntfile.js', 'couchapp/**/*.js'],
+			options: {
+				ignores: [
+					'couchapp/*/lib/*',
+					'couchapp/trips/_attachments/scripts/regression.js',
+					'couchapp/trips/_attachments/scripts/leaflet.js',
+					'couchapp/triggerjob/**',
+				],
+				esversion: 6,
+				//strict : 'implied',
+				laxcomma: true,
+				globals: {
+					couch: true
+				}
+			}
+		},
+		watch: {
+			files: ['<%= jshint.files %>'],
+			tasks: ['build']
+		},
+		'couch-compile': {
+			app: {
+				files: {
+					'bin/alldata.json': 'couchapp/alldata',
+					'bin/licenses.json': 'couchapp/licenses',
+					//'bin/triggerjob.json' : 'couchapp/triggerjob/*',
+					'bin/trips.json': 'couchapp/trips',
+				}
+			}
+		},
+		'couch-push': {
+			//options: {user: 'karin',pass: 'secure'},
+			dev: {
+				files: [{
+						dest: 'http://localhost:8080/fish',
+						src: 'bin/alldata.json'
+					}, {
+						dest: 'http://localhost:8080/fish',
+						src: 'bin/licenses.json'
+					},
+					//{dest:'http://localhost:8080/fish', src:'bin/triggerjob.json'},
+					{
+						dest: 'http://localhost:8080/fish',
+						src: 'bin/trips.json'
+					}
+				]
+			},
+			prod: {
+				options: grunt.config.process('<%= prodTarget %>'),
+				files: [{
+						dest: grunt.config.get('prodTarget').url,
+						src: 'bin/alldata.json'
+					}, {
+						dest: grunt.config.get('prodTarget').url,
+						src: 'bin/licenses.json'
+					},
+					//{dest: grunt.cofig.get('prodTarget').url, src:'bin/triggerjob.json'},
+					{
+						dest: grunt.config.get('prodTarget').url,
+						src: 'bin/trips.json'
+					}
+				]
+			}
+		},
+		mochaTest: {
+			test: {
+				options: {
+					reporter: 'spec',
+					quiet: false, // Optionally suppress output to standard out (defaults to false) 
+					clearRequireCache: false // Optionally clear the require cache before running tests (defaults to false) 
+				},
+				src: ['test/**/*.js']
+			}
+		}
+	});
+
+
+	grunt.loadNpmTasks('grunt-check-dependencies');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-exec');
+	grunt.loadNpmTasks('grunt-couch');
+	grunt.loadNpmTasks('grunt-shell');
+	grunt.loadNpmTasks('grunt-mocha-test');
+
+	grunt.registerTask('default', ['build']);
+	grunt.registerTask('test', ['mochaTest']);
+	grunt.registerTask('config', [
+		'checkDependencies',
+		'couch-configure',
+	]);
+	grunt.registerTask('build', [
+		'checkDependencies',
+		'jshint',
+		'couch',
+	]);
+
 };
-
-
--- test/selenium.js
+++ test/selenium.js
@@ -5,31 +5,40 @@
  * https://code.tutsplus.com/tutorials/headless-functional-testing-with-selenium-and-phantomjs--net-30545
  */
 
-describe('Test example.com', function(){
-    before(function(done) {
-        client.init().url('http://example.com', done);
-    });
+var assert = require("assert");
 
-    describe('Check homepage', function(){
-        it('should see the correct title', function(done) {
-            client.getTitle(function(err, title){
-                expect(title).to.have.string('Example Domain');
-                done();
-            });
-        });
+var client = require("webdriverjs").remote({
+	desiredCapabilities:{
+		browserName:'phantomjs'
+	},
+	logLevel:'silent/'
+});
 
-        it('should see the body', function(done) {
-            client.getText('p', function(err, p){
-                expect(p).to.have.string(
-                    'for illustrative examples in documents.'
-                );
-                done();
-            })
-        });
-    });
+describe('Test example.com', function() {
+	before(function(done) {
+		client.init().url('http://example.com', done);
+	});
 
-    after(function(done) {
-        client.end();
-        done();
-    });
+	describe('Check homepage', function() {
+		it('should see the correct title', function(done) {
+			client.getTitle(function(err, title) {
+				assert.ok(!err, 'Did not receive and errror');
+				assert.ok(title.includes('Example'));
+				done();
+			});
+		});
+
+		it('should see the body', function(done) {
+			client.getText('p', function(err, p) {
+				assert.ok(!err);
+				assert.ok(p.include('for illustrative examples in documents.'));
+				done();
+			});
+		});
+	});
+
+	after(function(done) {
+		client.end();
+		done();
+	});
 });
